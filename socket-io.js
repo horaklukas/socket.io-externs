e606077a5912c9fedba12cae5df955a6e941df35
Lukas Horak <lhorak@retia.cz>
2014-02-12 09:51:27 +0100

    Added SocketNamespace and some other significant methods of Socket object

--- socket-io.js
+++ socket-io.js
@@ -14,6 +14,17 @@ io.connect = function(uri, options) {};
 /** @constructor*/
 var Socket = function() {};
 
+/**
+ * @type {Object.<string,SocketNamespace>}
+ */
+Socket.prototype.namespaces;
+
+/** 
+ * @param {function()=} fn
+ */
+Socket.prototype.connect = function(fn) {};
+
+
 /** 
  * @param {string} event
  * @param {function()} handler
@@ -25,3 +36,33 @@ Socket.prototype.on = function(event, handler) {};
  * @param {...} args Variable number of arguments for pass to event
  */
 Socket.prototype.emit = function(name, args) {};
+
+/** @constructor*/
+var SocketNamespace = function() {};
+
+/** 
+ * @type {Socket}
+ */
+SocketNamespace.prototype.socket;
+
+/** 
+ * @param {string} event
+ * @param {function(...)} handler
+ */
+SocketNamespace.prototype.on = function(event, handler) {};
+
+/**
+ * @param {string} name Name of event to emit
+ * @param {...} args Variable number of arguments for pass to event
+ */
+SocketNamespace.prototype.emit = function(name, args) {};
+
+/**
+ * Method for disconnecting the channel
+ */
+SocketNamespace.prototype.disconnect = function() {};
+
+/**
+ * @param {Object} data Packet data
+ */
+SocketNamespace.prototype.packet = function(data) {};
